8c19b283c15514f81c9219bff48ced1884e16a04
Truong Le <lexuantruonghcm@gmail.com>
2017-10-16 01:24:36 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import 'hammerjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule, MatDialogModule, MatTooltipModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,22 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    // flex-layout module
+    FlexLayoutModule,
+
+    // supper route module
+    AppRoutingModule,
+
+    // material module
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
